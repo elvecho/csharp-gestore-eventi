7f0082a62cffe9017d0bf56e89753fa8f88b2e3b
elvecho <100934190+elvecho@users.noreply.github.com>
2022-04-22 17:57:06 +0200

    commit

---
+++ GestoreEventi/Class3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+namespace GestoreEventi
+
+    internal class Conferenza 
+    {
+        private string relatore;
+        private double prezzo;
+
+        public Conferenza(string Relatore, double prezzo) : base(string titolo, DateTime data, int capienzamax)
+    }
+}*/
